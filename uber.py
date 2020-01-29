01904ab00cace3f95cfe729322427c8cb2ec6524
Ponmalar20 <60420202+Ponmalar20@users.noreply.github.com>
2020-01-29 14:48:49 +0530

    Add files via upload

---
+++ uber.py
@@ -0,0 +1,15 @@
+def func(a,b):
+    if src=="avadi" and des=="redhills":
+      print("the amount is 100 ")
+    elif src=="avadi"  and des=="ponnamalle":
+      print("the amt is 80")
+    elif src=="ponnmalle" and des=="redhills":
+        print("the amt is 30")
+    else:
+        print("invalid area")
+
+src=input("enter source")
+des=input("enter dest")
+func(src,des)
+
+        
